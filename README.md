d0b88d377e0b84805a0af0947660b1069a56d4fc
mamorky <andres_ball@hotmail.com>
2017-12-10 14:48:18 +0100

    Add README + Comments

---
+++ README.md
@@ -0,0 +1,66 @@
+﻿
+
+**Trabajo ACDA 1º Trimestre**
+============================================
+
+>**<h3><b>Descarga de imagenes  y frases</b><h3>**
+
+
+En este ejercio se descargan las imagenes y las frases desde el servidor alumnos.mobi. En un fichero se encontrarán las imagenes , mientras que en el otro fichero estarán las frases. Esto se mostrarán de forma automática.
+
+<br/>
+>**<i>Estructura de Métodos Usados</i>**
+
+<br/>
+>Clase Principal
+
+>![enter image description here](https://lh3.googleusercontent.com/-tNIJ3Ll1qA8/Wi0wQ8uS7PI/AAAAAAAAJbY/cl0_WQVDiccJOKA66FBJNWZkqYHPeDRzwCLcBGAs/s2000/m%25C3%25A9todos.png "métodos.png")
+
+<br/>
+>Hilo de descarga de enlaces y frases
+
+>![enter image description here](https://lh3.googleusercontent.com/-r_DW9vtVEdU/Wi0xCkg6oRI/AAAAAAAAJbk/DQ5Qfy5QHIIXdfVeftNrUBdUbIoI4sLWACLcBGAs/s2000/metodosHilo.png "metodosHilo.png")
+
+<br/>
+>Hilo de descarga de imágenes
+
+>![enter image description here](https://lh3.googleusercontent.com/32FIm0bHOyhfb6_VBC-u1_G_KKsidGpL-QWSynD26xqOmwkTtOBR2RJGSGZgaYhVWXcZQG7sYnT8=s2000 "HiloDescargaImagenes.png")
+
+<br/>
+>**Observaciones**
+
+No he utilizado métodos asíncronos puesto que si usaba estos tenia un problema y es que el hilo principal seguía ejecutandose con lo cual lo que he hecho a sido crear dos hilos uno que descarga los enlaces y las frases y el otro que se encarga de descargar las imagenes, y para que el hilo principal no continue este llama al método esperar de los hilos("Este método llama al método join para que el hilo principal espere a que este hilo termine").
+
+Tampoco se han usado librerías externas todo esta realizado con las librerías internas.
+
+\#Las Imagenes se descargan no se leen desde internet. Estas se guardan como Bitmap en el ArrayList de images.
+
+>**Problemas Encontrados**
+
+Estos son alguno de los problemas que me he encontrado:
+
+ - En un hilo que no sea el hilo principal no se pueden crear elementos View con lo cual un hilo que no sea el principal no puede crear ni un **ProgressDialog** ni un **Toast**.
+ 
+ - **Conexión con el servidor** No he conseguido guardar el contenido de los errores en el fichero errores del servidor 
+
+>**Soluciones a los Problemas**
+
+Para el problema de los views en encontrado las siguientes soluciones:
+
+- En vez de mostrar un ProgressDialog lo que hago es **cambiar el texto del botón de descarga** para que este muestre que se esta descargando
+
+- En el caso de los Toast lo que hago es crear una variable de String llamada **mensajeExcepcionHilos** que alamacenará el error que se a producido en el hilo en caso de producirse. Cuando el hilo externo termina el hilo principal preguntara si este es null o no si no es null es por que se produjo un excepción y se mostrará el toast. La variable **mensajeExcepcionHilos** se inicializará a null si este fuera distinto después de mostrar el Toast.
+
+- Para el problema de guardar los errores aun no he encontrado una solución 
+
+>**Mejoras en la App**
+
+- Se ha añadido la posibilidad de que si volvemos a pulsar el botón esta pare el proceso de pasar las imagenes y reinicialice los ArrayList y las banderas por si queremos volver a descargar. Esto se hace en el método **cerrarManejador**.
+
+- Imagenes almacenadas en el dispositivo, si perdemos la conexión a internet estas deberían seguir pasandose sin problema a pesar de perder la conexión.
+<br/>
+
+>**Imagen de muestra de la APP**
+
+![enter image description here](https://lh3.googleusercontent.com/X2mVqY_u5KF3G42iOLiqpYYzvty3J9JmNmB61kxPK3R_85rgVaOGWs6yoW8PwkmcLbQyrJ4NKLbk=s800 "TrabajoFichero.png")
+
--- app/src/main/java/com/example/mamorky/trabajoficheros/DescargaImagenesFrases.java
+++ app/src/main/java/com/example/mamorky/trabajoficheros/DescargaImagenesFrases.java
@@ -28,29 +28,36 @@ import java.util.ArrayList;
  * Created by mamorky on 25/11/17.
  */
 
-public class DescargaImagenes extends AppCompatActivity implements View.OnClickListener {
+public class DescargaImagenesFrases extends AppCompatActivity implements View.OnClickListener {
 
+    //Imagen que se muestra cuando se produce un error
     public static Drawable imgError;
 
+    //Views del layout
     private Button btnDescarga;
     private EditText edtFichImagenes, edtFichFrases;
     private ImageView imgIMagen;
     private TextView txvFrase;
 
+    //ArrayList donde se guardará frases,enlaces e imagenes
     private static ArrayList<String> imagesText;
     private static ArrayList<Bitmap> images;
     private static ArrayList<String> frases;
 
+    //Banderitas que usaremos para recorrer los ArrayList
     private static int flagImg = 0;
     private static int flagFrase = 0;
     private static int intervalo = 0;
 
-    private static final String URLERROR = "alumno.mobi/~alumno/superior/bujalance/errores.txt";
+    //Ruta donde se encuentra el archivo de Errores
+    private static final String URLERROR = "http://alumno.mobi/~alumno/superior/bujalance/errores.txt";
 
+    //Manejador,runable y booleano que indica si el handler esta corriendo
     private Handler handler;
     private Runnable runnable;
     private static boolean correHandler = false;
 
+    //Cuando se produce una excepción en un hilo el mensaje se guarda aquí y este se muestra en un Toast
     public static String mensajeExcepcionHilos;
 
     @Override
@@ -81,9 +88,19 @@ public class DescargaImagenes extends AppCompatActivity implements View.OnClickL
         InputStream streamIntervalo = getApplicationContext().getResources().openRawResource(R.raw.intervalo);
     }
 
+    /**
+     * Método que se encargara de llamar al hilo que descarga todas los enlaces de las imagenes y las frases
+     * @return true si consigue descargar con existo, si no false
+     * */
     public boolean descargaEnlacesFrases() {
+
+        //Un hilo no me permite crear un ProgressDialog con lo que mi alternativa es cambiar el texto del botón
         btnDescarga.setText("Descarga de enlaces y frases ...");
+
+        //Se crea el hilo
         HiloLeerArchivos hiloLee = new HiloLeerArchivos(edtFichImagenes.getText().toString(), edtFichFrases.getText().toString(), imagesText, frases);
+
+        //Hacemos que el hilo principal esperé a que este haya descargado todos los enlaces para continuar
         hiloLee.Esperame();
 
         if (mensajeExcepcionHilos != null) {
@@ -96,10 +113,19 @@ public class DescargaImagenes extends AppCompatActivity implements View.OnClickL
         return true;
     }
 
+    /**
+     * Método que llama al hilo de descarga de imagenes
+     * @return true si descarga con existo
+     * */
     public boolean descargarImagenes() {
+
+        //Es la alternativa usada al ProgressDialog
         btnDescarga.setText("Descarga de imagenes ...");
+
+        //Se crea el hilo de descarga de imagenes
         HiloDescargaImagenes hiloDescargaImagenes = new HiloDescargaImagenes(imagesText, images);
 
+        //Se le dice al hilo principal que espera a la descarga de imagenes
         hiloDescargaImagenes.Esperame();
 
         if (mensajeExcepcionHilos != null) {
@@ -112,6 +138,9 @@ public class DescargaImagenes extends AppCompatActivity implements View.OnClickL
         return true;
     }
 
+    //**
+    // Método que obtiene el intervalo del archivo intervalos.txt
+    // */
     private void obtenerIntervalo() {
         //Creamos un InputStream con el archivo intervalo que es el que tendrá el intervalo de tiempo entre imagen e imagen
         InputStream streamIntervalo = getApplicationContext().getResources().openRawResource(R.raw.intervalo);
@@ -125,9 +154,9 @@ public class DescargaImagenes extends AppCompatActivity implements View.OnClickL
         } catch (IOException e) {
             //En caso de haber excepción establecemos 1 segundo como el valor del intervalo
             intervalo = 1;
-            DescargaImagenes.guardarErrores(e.getMessage());
+            DescargaImagenesFrases.guardarErrores(e.getMessage());
             e.printStackTrace();
-            Toast.makeText(DescargaImagenes.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            Toast.makeText(DescargaImagenesFrases.this, e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
 
@@ -143,6 +172,10 @@ public class DescargaImagenes extends AppCompatActivity implements View.OnClickL
         txvFrase.setText(nextFrase);
     }
 
+    /**
+     * El manejador a de ser cerrado pues este es quien se encarga de cerrarlo, es decir, elimina el proceso del hanlder
+     * Este método también se encargará eliminar el contenido de las listas, y de volver a inicializar las banderitas
+     * */
     private void cerrarManejador() {
         if (handler != null)
             handler.removeCallbacks(runnable);
@@ -151,15 +184,24 @@ public class DescargaImagenes extends AppCompatActivity implements View.OnClickL
         //Cuando paramos se borran todas las imagenes y frases
         images.clear();
         imagesText.clear();
-        imagesText.clear();
+        frases.clear();
+
+        //Tambien ponemos la banderita a cero
+        flagFrase = 0;
+        flagImg = 0;
 
         btnDescarga.setText("DESCARGAR");
     }
 
+    /**
+     * Recorre las imagenes crea un manejador y un runnable que se le asignará a este con un retardo
+     * que es el intervalo obtenido del texto
+     * */
     private void recorrerImagenes() {
-        if (images.size() > 0) {
+            //Nos muestra un mensaje que si llegamos a este punto es por que la descarga se realizo con exito
             Toast.makeText(this, "Descargado", Toast.LENGTH_LONG).show();
 
+            //Llamamos al método que obtiene el intervalo
             obtenerIntervalo();
 
             try {
@@ -173,32 +215,43 @@ public class DescargaImagenes extends AppCompatActivity implements View.OnClickL
                     }
                 };
 
-                handler.postDelayed(runnable, intervalo * 1000);
+                //Asiganmos al manejador el runnable
+                handler.post(runnable);
+
+                //Decimos que el handler esta corriendo
                 correHandler = true;
+
+                //Y cambiamos el texto del botón por parar para parar el proceso cuando nosotros pulsemos sobre este.
                 btnDescarga.setText("PARAR");
             } catch (Exception e) {
-                Toast.makeText(DescargaImagenes.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                Toast.makeText(DescargaImagenesFrases.this, e.getMessage(), Toast.LENGTH_LONG).show();
+
+                //Si se produce una excepción cerramos el manejador para que este no siga consumiendo
                 cerrarManejador();
             }
-        } else
-            cerrarManejador();
     }
 
-    /**
-     * Sin soculción
-     *
-     * @param error
+    /**SIN SOLUCIÓN
+     * ------------
+     * Este es el método que debería guardar los errores en el pero no he conseguido hayar la solución
+     * @param error que se produce como string
      */
     public static void guardarErrores(String error) {
         try {
+            //Pasa a bytes el error recibido
             byte[] bytes = error.getBytes();
 
-            URL url = new URL("http://" + URLERROR);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setDoOutput(true);
-            DataOutputStream out = new DataOutputStream(urlConnection.getOutputStream());
+            //Crea la URL
+            URL url = new URL(URLERROR);
 
+            //Crea la concexión con la clase HTTPURLConection
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            //La decimos que se puedan poner contenido en esta dirección
+             urlConnection.setDoOutput(true);
+
+            //Creamos un DataOutputStream que recibirá el stream de la conexión y  escribirá en este
+            DataOutputStream out = new DataOutputStream(urlConnection.getOutputStream());
             out.write(bytes);
             out.flush();
         } catch (Exception e) {
@@ -208,21 +261,27 @@ public class DescargaImagenes extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void onClick(View v) {
+        //Si se pulsa el botón descarga y no esta andando el handler ...
         if (v == btnDescarga && !correHandler) {
 
-            //Se llama a los métodos de descarga y si alguno falla se deja de ejecutar el onClick()
+            //... Se llama a los métodos de descarga y si alguno falla se deja de ejecutar el onClick()
             if (!descargaEnlacesFrases() || !descargarImagenes())
                 return;
 
+            //... Se recorren las imagenes
             recorrerImagenes();
+
+        //Si se pulsa el botón de descarga y el handler esta corriendo, este elimina el proceso
         } else if (v == btnDescarga && correHandler) {
             cerrarManejador();
         }
     }
 }
 
+/**
+ * Hilo que se encarga de descargar los enlaces de las imagenes y las frases
+ * */
 class HiloLeerArchivos implements Runnable {
-
     String rutaImagenes;
     String rutaFrases;
     ArrayList<String> images;
@@ -235,74 +294,102 @@ class HiloLeerArchivos implements Runnable {
         this.images = images;
         this.frases = frases;
         hilo = new Thread(this);
+
+        //El hilo se debe de iniciar en el constructor debido a que implementamos la interfaz runnable
         hilo.start();
     }
 
     @Override
     public void run() {
+        //Llamamos al método LeerFicheroURL que se encarga de  el fichero del servidor
+
+        //Se descargan los enlaces de las imagenes y se guardan en el ArrayList de imagenes
         leerFicheroUrl(rutaImagenes, images);
+
+        //Se descargan las frases y se guardan en el ArrayList de frases
         leerFicheroUrl(rutaFrases, frases);
     }
 
+    //Método que usará el main para hacer que este lo espere
     public void Esperame() {
         try {
             hilo.join();
         } catch (InterruptedException e) {
-            DescargaImagenes.guardarErrores(e.getMessage());
+            DescargaImagenesFrases.guardarErrores(e.getMessage());
             e.printStackTrace();
         }
     }
 
     /**
-     * Método que recibe la ruta donde se encuentra un fichero lo lee y lo guarda en la lista de string pasada
+     * Método que recibe la ruta donde se encuentra un fichero del servidor, lo lee y lo guarda en la lista de string pasada
+     * @param ruta -> Es la ruta donde se encuentra el fichero
+     * @param guardado -> Donde se guardarán los string leidos
      */
     public void leerFicheroUrl(String ruta, ArrayList<String> guardado) {
         String linea = "";
         try {
-            //Recibimos la URL
+            //Creamos una URL a partir de la ruta pasada
             URL url = new URL(ruta);
 
             //Creamos un bufferReader con lo que leeremos el fichero de la web
             BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
 
+            //Leemos todas las lineas del fichero
             while ((linea = in.readLine()) != null) {
                 guardado.add(linea);
             }
+
+            //Cerramos el fichero
             in.close();
         } catch (Exception e) {
-            DescargaImagenes.guardarErrores(e.getMessage());
-            DescargaImagenes.mensajeExcepcionHilos = e.getMessage();
+            //Llama al método que guarda los errores
+            DescargaImagenesFrases.guardarErrores(e.getMessage());
+
+
+            DescargaImagenesFrases.mensajeExcepcionHilos = e.getMessage();
             e.printStackTrace();
         }
     }
 }
 
+/**
+ * Hilo que se encargará de descargar las imagenes
+ * */
 class HiloDescargaImagenes implements Runnable {
     ArrayList<String> imagesText;
     ArrayList<Bitmap> images;
     Thread hilo;
 
+    //El constructor del hilo recibe un ArrayList de string con la ruta donde estas se encuentran y
+    // un ArrayList de Bitmap donde estas se guaradarán
     public HiloDescargaImagenes(ArrayList<String> imagesText, ArrayList<Bitmap> images) {
         this.imagesText = imagesText;
         this.images = images;
         hilo = new Thread(this);
+
+        //Iniciamos el hilo dentro del constructor
         hilo.start();
     }
 
     @Override
     public void run() {
+        //El run llama al método de descarga de imagenes
         descargaImagenes();
     }
 
+    //Al implementar la interfaz runnable es necesario crear un método  para decirle al hilo que se espere
     public void Esperame() {
         try {
             hilo.join();
         } catch (InterruptedException e) {
-            DescargaImagenes.guardarErrores(e.getMessage());
+            DescargaImagenesFrases.guardarErrores(e.getMessage());
             e.printStackTrace();
         }
     }
 
+    /**
+     * Es el método encargado de la descarga de imagenes
+     * */
     private void descargaImagenes() {
         for (int i = 0; i < imagesText.size(); i++) {
             Bitmap bitmap = null;
@@ -316,12 +403,12 @@ class HiloDescargaImagenes implements Runnable {
                 //Añadimos la imagen al array de imagenes a mostrar
                 images.add(bitmap);
             } catch (FileNotFoundException e) {
-                DescargaImagenes.guardarErrores(e.getMessage());
-                DescargaImagenes.mensajeExcepcionHilos = e.getMessage();
+                DescargaImagenesFrases.guardarErrores(e.getMessage());
+                DescargaImagenesFrases.mensajeExcepcionHilos = e.getMessage();
             } catch (Exception e) {
-                DescargaImagenes.guardarErrores(e.getMessage());
+                DescargaImagenesFrases.guardarErrores(e.getMessage());
                 //En caso de haber una excepción en alguna de las imagenes se pondrá una imagen de error
-                Bitmap bitmapError = ((BitmapDrawable) DescargaImagenes.imgError).getBitmap();
+                Bitmap bitmapError = ((BitmapDrawable) DescargaImagenesFrases.imgError).getBitmap();
                 images.add(bitmapError);
                 e.printStackTrace();
             }
